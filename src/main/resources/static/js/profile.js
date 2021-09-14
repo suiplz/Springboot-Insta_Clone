0a3230ea67c60c9400ca48b2bf42104e0aa15e74
acepade231@gmail.com <acepade231@gmail.com>
2021-09-14 23:35:38 +0900

    0912

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -59,7 +59,7 @@ function subscribeInfoModalOpen(pageUserId) {
 }
 
 function getSubscribeModalItem(u) {
-    let item = `<div class="subscribe__item" id="subscribeModalItem-${u.id}">
+    let item = `<div class="subscribe__item" id="subscribeModalItem-${u.userId}">
 	<div class="subscribe__img">
 		<img src="/upload/${u.profileImageUrl}" onerror="this.src='/images/person.jpeg'"/>
 	</div>
@@ -69,9 +69,9 @@ function getSubscribeModalItem(u) {
     <div class="subscribe__btn">`;
     if(!u.equalUserState){ // 동일 유저가 아닐 때 버튼이 만들어져야함
         if(u.subscribeState){ // 구독한 상태
-            item += `<button className="cta blue" onClick="toggleSubscribeModal(this)">구독취소</button>`;
+            item += `<button class="cta blue" onClick="toggleSubscribe(${u.userId}, this)">구독취소</button>`;
     }else{ // 구독 안한 상태
-            item += `<button className="cta blue" onClick="toggleSubscribeModal(this)">구독하기</button>`;
+            item += `<button class="cta" onClick="toggleSubscribe(${u.userId}, this)">구독하기</button>`;
         }
     }
     item +=`
@@ -82,18 +82,7 @@ function getSubscribeModalItem(u) {
 }
 
 
-// (3) 구독자 정보 모달에서 구독하기, 구독취소
-function toggleSubscribeModal(obj) {
-    if ($(obj).text() === "구독취소") {
-        $(obj).text("구독하기");
-        $(obj).toggleClass("blue");
-    } else {
-        $(obj).text("구독취소");
-        $(obj).toggleClass("blue");
-    }
-}
-
-// (4) 유저 프로파일 사진 변경 (완)
+// (3) 유저 프로파일 사진 변경 (완)
 function profileImageUpload() {
     $("#userProfileImageInput").click();
 
@@ -115,7 +104,7 @@ function profileImageUpload() {
 }
 
 
-// (5) 사용자 정보 메뉴 열기 닫기
+// (4) 사용자 정보 메뉴 열기 닫기
 function popup(obj) {
     $(obj).css("display", "flex");
 }
@@ -125,17 +114,17 @@ function closePopup(obj) {
 }
 
 
-// (6) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
+// (5) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
 function modalInfo() {
     $(".modal-info").css("display", "none");
 }
 
-// (7) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+// (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
 function modalImage() {
     $(".modal-image").css("display", "none");
 }
 
-// (8) 구독자 정보 모달 닫기
+// (7) 구독자 정보 모달 닫기
 function modalClose() {
     $(".modal-subscribe").css("display", "none");
     location.reload();
